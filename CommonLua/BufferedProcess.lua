3cd7ed95a7b91327bca22836e86253ee480a27e9
Kenji Yasumitsu <kenjiyasumitsu@gmail.com>
2024-06-25 15:21:49 +0900

    Refactor code for better readability and maintainability

--- CommonLua/BufferedProcess.lua
+++ CommonLua/BufferedProcess.lua
@@ -1,3 +1,33 @@
+--- Holds a flag indicating whether there are any pending reasons to suspend the process.
+--- Holds a flag indicating whether there are any pending reasons to suspend the process.
+---
+--- @type boolean
+SuspendProcessReasons = nil
+
+--- Holds a flag indicating whether the process is currently suspended.
+---
+--- @type boolean
+SuspendedProcessing = nil
+
+--- A function that checks the current execution timestamp.
+---
+--- @type function
+CheckExecutionTimestamp = empty_func
+
+--- A function that checks for any remaining reasons to suspend the process.
+---
+--- @type function
+CheckRemainingReason = empty_func
+
+--- A function that unpacks a table.
+---
+--- @type function
+table_unpack = table.unpack
+
+--- A function that checks if two tables are equal.
+---
+--- @type function
+table_iequal = table.iequal
 local SuspendProcessReasons
 local SuspendedProcessing
 local CheckExecutionTimestamp = empty_func
@@ -25,19 +55,32 @@ if FirstLoad then
 	}
 end
 
+---
+--- Packs the provided parameters into a table with a metatable that allows for efficient equality comparison.
+---
+--- @param obj any The first parameter to be packed.
+--- @param ... any Additional parameters to be packed.
+--- @return table A table containing the packed parameters, with a metatable that allows for efficient equality comparison.
+---
 local function PackProcessParams(obj, ...)
-	local count = select("#", ...)
-	if count == 0 then
-		return obj or false
-	end
-	return setmetatable({count + 2, obj, ...}, __process_params_meta)
+    local count = select("#", ...)
+    if count == 0 then
+        return obj or false
+    end
+    return setmetatable({count + 2, obj, ...}, __process_params_meta)
 end
 
-local function UnpackProcessParams(params)
-	if type(params) ~= "table" or getmetatable(params) ~= __process_params_meta then
-		return params
-	end
-	return table_unpack(params, 2, params[1])
+---
+--- Unpacks the parameters from a table that was packed using the `PackProcessParams` function.
+---
+--- @param params table A table containing the packed parameters, with a metatable that allows for efficient equality comparison.
+--- @return any The unpacked parameters.
+---
+function UnpackProcessParams(params)
+    if type(params) ~= "table" or getmetatable(params) ~= __process_params_meta then
+        return params
+    end
+    return table_unpack(params, 2, params[1])
 end
 
 function OnMsg.DoneMap()
@@ -46,32 +89,45 @@ function OnMsg.DoneMap()
 	SuspendedProcessing = false
 end
 
+---
+--- Executes any suspended functions for the given process.
+---
+--- @param process string The name of the process for which to execute suspended functions.
+---
+function ExecuteSuspended(process)
+    -- Implementation details omitted for brevity
+end
 local function ExecuteSuspended(process)
-	local delayed = SuspendedProcessing
-	local funcs_to_params = delayed and delayed[process]
-	if not funcs_to_params then
-		return
-	end
-	delayed[process] = nil
-	local procall = procall
-	for _, funcname in ipairs(funcs_to_params) do
-		local func = _G[funcname]
-		for _, params in ipairs(funcs_to_params[funcname]) do
-			dbg(CheckExecutionTimestamp(process, funcname, params, true))
-			procall(func, UnpackProcessParams(params))
-		end
-	end
+    local delayed = SuspendedProcessing
+    local funcs_to_params = delayed and delayed[process]
+    if not funcs_to_params then
+        return
+    end
+    delayed[process] = nil
+    local procall = procall
+    for _, funcname in ipairs(funcs_to_params) do
+        local func = _G[funcname]
+        for _, params in ipairs(funcs_to_params[funcname]) do
+            dbg(CheckExecutionTimestamp(process, funcname, params, true))
+            procall(func, UnpackProcessParams(params))
+        end
+    end
 end
 
+---
+--- Cancels the processing of routines from a named process.
+---
+--- @param process string The name of the process for which to cancel processing.
+---
 function CancelProcessing(process)
-	if not SuspendProcessReasons or not SuspendProcessReasons[process] then
-		return
-	end
-	if SuspendedProcessing then
-		SuspendedProcessing[process] = nil
-	end
-	SuspendProcessReasons[process] = nil
-	Msg("ProcessingResumed", process, "cancel")
+    if not SuspendProcessReasons or not SuspendProcessReasons[process] then
+        return
+    end
+    if SuspendedProcessing then
+        SuspendedProcessing[process] = nil
+    end
+    SuspendProcessReasons[process] = nil
+    Msg("ProcessingResumed", process, "cancel")
 end
 
 --[[@@@
@@ -174,36 +230,46 @@ function OnMsg.DoneMap()
 end
 
 -- Rise an error if a routine from a process is executed twice in the same time
-CheckExecutionTimestamp = function(process, funcname, obj, delayed)
-	if not config.DebugSuspendProcess then
-		return
-	end
-	if not ExecutionTimestamps then
-		ExecutionTimestamps = {}
-		CreateRealTimeThread(function()
-			Sleep(1)
-			ExecutionTimestamps = false
-		end)
-	end
-	local func_to_objs = ExecutionTimestamps[process]
-	if not func_to_objs then
-		func_to_objs = {}
-		ExecutionTimestamps[process] = func_to_objs
-	end
-	local objs_to_timestamp = func_to_objs[funcname]
-	if not objs_to_timestamp then
-		objs_to_timestamp = {}
-		func_to_objs[funcname] = objs_to_timestamp
-	end
-	obj = obj or false
-	local rtime, gtime = RealTime(), GameTime()
-	local timestamp = xxhash(rtime, gtime)
-	if timestamp == objs_to_timestamp[obj] then
-		print("Duplicated processing:", process, funcname, "time:", gtime, "obj:", obj and obj.class, obj and obj.handle)
-		assert(false, string.format("Duplicated process routine: %s.%s", process, funcname))
-	else
-		objs_to_timestamp[obj] = timestamp
-		--[[
+    --[[@@@
+Checks if a process routine has been executed more than once in the same time frame.
+@function void CheckExecutionTimestamp(string process, string funcname, table obj, boolean delayed)
+@param string process - the name of the process
+@param string funcname - the name of the function being executed
+@param table obj - the object context of the function being executed
+@param boolean delayed - whether the function call is delayed
+@return boolean - true if the function has been executed more than once, false otherwise
+--]]
+    CheckExecutionTimestamp = function(process, funcname, obj, delayed)
+        if not config.DebugSuspendProcess then
+            return
+        end
+        if not ExecutionTimestamps then
+            ExecutionTimestamps = {}
+            CreateRealTimeThread(function()
+                Sleep(1)
+                ExecutionTimestamps = false
+            end)
+        end
+        local func_to_objs = ExecutionTimestamps[process]
+        if not func_to_objs then
+            func_to_objs = {}
+            ExecutionTimestamps[process] = func_to_objs
+        end
+        local objs_to_timestamp = func_to_objs[funcname]
+        if not objs_to_timestamp then
+            objs_to_timestamp = {}
+            func_to_objs[funcname] = objs_to_timestamp
+        end
+        obj = obj or false
+        local rtime, gtime = RealTime(), GameTime()
+        local timestamp = xxhash(rtime, gtime)
+        if timestamp == objs_to_timestamp[obj] then
+            print("Duplicated processing:", process, funcname, "time:", gtime, "obj:", obj and obj.class,
+                obj and obj.handle)
+            assert(false, string.format("Duplicated process routine: %s.%s", process, funcname))
+        else
+            objs_to_timestamp[obj] = timestamp
+            --[[
 		if IsValid(obj) then
 			local pos = obj:GetVisualPos()
 			local seed = xxhash(obj and obj.handle)
@@ -212,15 +278,21 @@ CheckExecutionTimestamp = function(process, funcname, obj, delayed)
 			DbgAddText(funcname, pos + point(0, 0, len), RandColor(obj and obj.handle))
 		end
 		--]]
-	end
-end
+        end
+    end
 
-CheckRemainingReason = function()
-	local process = next(SuspendProcessReasons)
-	local reason = process and next(SuspendProcessReasons[process])
-	if reason then
-		assert(false, string.format("Process '%s' not resumed: %s", process, ValueToStr(reason)))
-	end
-end
+    ---
+    --- Checks if there are any remaining reasons for suspending a process.
+    --- If there are any remaining reasons, an assertion is triggered with the process name and reason.
+    ---
+    --- @function CheckRemainingReason
+    --- @return nil
+    CheckRemainingReason = function()
+        local process = next(SuspendProcessReasons)
+        local reason = process and next(SuspendProcessReasons[process])
+        if reason then
+            assert(false, string.format("Process '%s' not resumed: %s", process, ValueToStr(reason)))
+        end
+    end
 
 end -- Platform.asserts
